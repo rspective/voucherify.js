b9f3c00759b125e98e4cbba4d0e9c490ea36ef5a
Łukasz Wiktor <lukasz.wiktor@gmail.com>
2016-04-14 13:14:38 +0200

    added sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,16 @@ var gulp        = require("gulp");
 var uglify      = require("gulp-uglify");
 var uglifycss   = require('gulp-uglifycss');
 var rename      = require("gulp-rename");
-var sequence    = require('gulp-sequence')
+var sequence    = require('gulp-sequence');
+var sourcemaps  = require('gulp-sourcemaps');
 
 gulp.task("build-js", function() {
   return gulp.src([ "lib/voucherify.js" ])
     .pipe(gulp.dest("./dist"))
+    .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(rename("voucherify.min.js"))
+    .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest("./dist"));
 });
 
@@ -18,8 +21,10 @@ gulp.task("build-js", function() {
 gulp.task("build-css", function() {
   return gulp.src([ "lib/voucherify.css" ])
     .pipe(gulp.dest("./dist"))
+    .pipe(sourcemaps.init())
     .pipe(uglifycss())
     .pipe(rename("voucherify.min.css"))
+    .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest("./dist"));
 });
 
