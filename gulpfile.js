35cf165b713c8724bb854677d670fa336716647a
Łukasz Wiktor <lukasz.wiktor@gmail.com>
2016-04-14 13:01:26 +0200

    remove version number from dist files (to meet cdn requirements)

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,25 @@
 "use strict";
 
-var util        = require("util");
 var gulp        = require("gulp");
 var uglify      = require("gulp-uglify");
 var uglifycss   = require('gulp-uglifycss');
 var rename      = require("gulp-rename");
 var sequence    = require('gulp-sequence')
-var config      = require("./package.json");
 
 gulp.task("build-js", function() {
-  var prefix = util.format("voucherify-%s", config.version);
-
   return gulp.src([ "lib/voucherify.js" ])
-    .pipe(rename(prefix + ".js"))
     .pipe(gulp.dest("./dist"))
     .pipe(uglify())
-    .pipe(rename(prefix + ".min.js"))
+    .pipe(rename("voucherify.min.js"))
     .pipe(gulp.dest("./dist"));
 });
 
 
 gulp.task("build-css", function() {
-  var prefix = util.format("voucherify-%s", config.version);
-
   return gulp.src([ "lib/voucherify.css" ])
-    .pipe(rename(prefix + ".css"))
     .pipe(gulp.dest("./dist"))
     .pipe(uglifycss())
-    .pipe(rename(prefix + ".min.css"))
+    .pipe(rename("voucherify.min.css"))
     .pipe(gulp.dest("./dist"));
 });
 
